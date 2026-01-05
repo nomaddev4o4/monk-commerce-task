7d70900932611e27fed80fb5d78bf33e0683327a
nomaddev4o4 <krishnakantanaik04@gmail.com>
2026-01-05 22:53:00 +0530

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <header>
+      <img className={styles.logo} src="logo.png" alt="logo" />
+      <h1 className={styles.title}>Monk Upsell & Cross-sell</h1>
+    </header>
+  );
+}
+
+export default Header;
